596acbf34470487e1550ecfc8b46f6b385b05f58
aboueloa <ayman.aboueloula@gmail.com>
2024-04-18 22:38:17 +0200

    finish working on services cards

--- app/components/ServiceCard.jsx
+++ app/components/ServiceCard.jsx
@@ -2,7 +2,28 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-const ServiceCard = ({imageUrl, title, href}) => {
+import { useState } from 'react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+  },
+  button: {
+    border: 'none',
+    background: 'none',
+    cursor: 'pointer',
+    display: 'block',
+    margin: '0 auto',
+  },
+};
+
+const ServiceCard = ({imageUrl, title, href, description}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
   return (
     <div>
         <div className ="h-80 md:h-xl rounded-t-xl relative group">
@@ -16,8 +37,20 @@ const ServiceCard = ({imageUrl, title, href}) => {
             <div className="overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
             </div>
          </div>
-         <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
+         <div className="center text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
             <h5 className='text-xl font-semibold mb-2'>{title}</h5>
+            <button onClick={toggleExpand} style={styles.button}>
+              {isExpanded ? <FaChevronUp /> : <FaChevronDown />}
+            </button>
+              {isExpanded ? (
+                <p>
+                  {description}
+                </p>
+              ) : (
+                <p>
+                </p>
+              )}
+
          </div>
     </div>
   )
--- app/components/ServiceSection.jsx
+++ app/components/ServiceSection.jsx
@@ -6,11 +6,24 @@ const services = [
     href: "#formation",
     image: "/images/f555.webp",
     title: "Formation",
+    description : (
+      <ul className='list-disc pl-2'>
+          <li>Radioprotection des travailleurs en imagerie médicale.</li>  
+          <li>Radioprotection des travailleurs en Radiothérapie.</li>
+      </ul>
+    ),
   },
   {
     href: "#equipements",
     image: "/images/f77.jpeg",
     title: "Equipements biomédicaux",
+    description : (
+      <ul className='list-disc pl-2'>
+          <li>Commercialisation des accessoires de la radioprotection en Médecine
+nucléaire, Radiothérapie, Radiologie.</li>  
+          <li>Commercialisation des dispositifs biomédicaux.</li>
+      </ul>
+    ),
   }
 ]
 
@@ -23,9 +36,9 @@ const ServiceSection = () => {
         <div className="grid md:grid-cols-2 gap-8 md:gap-12">
           {
             services.map((service, index) => (
-              <li key={index}>
-                <ServiceCard imageUrl={service.image} title={service.title} href={service.href} />
-              </li>
+              <div key={index}>
+                <ServiceCard imageUrl={service.image} title={service.title} href={service.href} description={service.description} />
+              </div>
             ))
           }
           
